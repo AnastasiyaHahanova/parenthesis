d939d0aef0689df6eb67a8be6a5d1183c04eacc5
AnastasiyaHahanova <anastasiya.hahanova@mail.ru>
2023-04-13 11:40:57 +0300

    Приложение верификации строки со скобками

--- src/Validator.php
+++ src/Validator.php
@@ -23,47 +23,26 @@ class Validator
 
     public static function containsUnclosedParenthesis(string $value, string $open, string $closed): bool
     {
-        if (!str_contains($value, $open) && !str_contains($value, $closed)) {
+        $check   = [];
+        $symbols = str_split($value);
+        if (empty($symbols)) {
             return false;
         }
 
-        if (str_contains($value, $open) && !str_contains($value, $closed) || str_contains($value, $closed) && !str_contains($value, $open)) {
+        if ($symbols[0] === $closed) {
             return true;
         }
 
-        while (str_contains($value, $open) && str_contains($value, $closed)) {
-            if (strpos($value, $closed) < strpos($value, $open)) {
+        foreach ($symbols as $symbol) {
+            if ($symbol === $open) {
+                $check[] = $open;
+            } elseif (empty($check)) {
                 return true;
+            } else {
+                array_pop($check);
             }
-
-            self::deleteSymbol($value, strpos($value, $open));
-            self::deleteSymbol($value, strpos($value, $closed));
         }
 
-        if (str_contains($value, $open) || str_contains($value, $closed)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public static function deleteSymbol(string &$str, int $position): void
-    {
-        if ($position === 0) {
-            $str = substr($str, 1);
-
-            return;
-        }
-
-        if ($position === strlen($str)) {
-            $str = substr($str, 0, strlen($str) - 1);
-
-            return;
-        }
-
-        $firstPart  = substr($str, 0, $position);
-        $secondPart = substr($str, $position + 1);
-
-        $str = $firstPart . $secondPart;
+        return !empty($check);
     }
 }
